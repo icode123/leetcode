5ed132200ef90ab1c216942acdd68b81ec924e1f
icode123 <yantian_123@126.com>
2013-06-10 22:41:37 +0800

    Update Two Sum.cpp

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -40,27 +40,30 @@ public:
     }
 };
 //o(n)
- int len = numbers.size();  
-        assert(len >= 2);             
-          
-        vector<int> ret(2, 0);  
-          
-        map<int, int> mapping;              // default all are 0  
-        vector<long long> mul(len, 0);  
-          
-        for(int i = 0; i < len; i++){  
-            mul[i] = (target - numbers[i]) * numbers[i];  
-              
-            if(mapping[mul[i]] > 0){        // not default 0  
-                if(numbers[i] + numbers[mapping[mul[i]] - 1] == target){  
-                    ret[0] = mapping[mul[i]];  
-                    ret[1] = i + 1;  
-                    break;  
-                }  
-                  
-            } else {  
-                mapping[mul[i]] = i + 1;    // larger than 0  
-            }  
-        }  
-          
-        return ret; 
+ class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        vector<int> ret;
+        if(numbers.size()<=1) return ret;
+        int len=numbers.size();
+        vector<long long> mul(len,0);
+        map<int,int> mapping;
+        for(int i=0;i<len;i++)
+        {
+            mul[i]=(target-numbers[i])*numbers[i];
+            if(mapping[mul[i]]>0)
+            {
+                if(target==numbers[i]+numbers[mapping[mul[i]]-1])
+                {
+                    ret.push_back(mapping[mul[i]]);
+                    ret.push_back(i+1);
+                    break;
+                }
+            }
+            else mapping[mul[i]]=i+1;
+        }
+        return ret;
+    }
+};
