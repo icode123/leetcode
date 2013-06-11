8ab34c7e4fcfaa5f37c67d78e46beeb72cbba342
icode123 <yantian_123@126.com>
2013-06-11 19:25:50 +0800

    Update String to Integer (atoi).cpp

--- String to Integer (atoi).cpp
+++ String to Integer (atoi).cpp
@@ -1,2 +1,30 @@
 //各种边界情况讨论
 
+class Solution {
+public:
+    int atoi(const char *str) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+         assert(str != NULL);        
+         while(isspace(*str))
+             str++;            
+         int sign = *str == '-' ? -1 : 1;         
+         if (*str == '-' || *str == '+')
+             str++;            
+         int ret = 0;
+         while(isdigit(*str))
+         {
+             int digit = *str - '0';            
+             if (INT_MAX / 10 >= ret)
+                 ret *= 10;
+             else
+                 return sign == -1 ? INT_MIN : INT_MAX;                 
+             if (INT_MAX - digit >= ret)
+                 ret += digit;
+             else
+                 return sign == -1 ? INT_MIN : INT_MAX;                
+             str++;
+         }         
+         return ret * sign;
+    }
+};
