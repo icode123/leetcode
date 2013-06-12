778efc407db60b9da193c5b5c07576ce09c0c972
icode123 <yantian_123@126.com>
2013-06-12 15:22:51 +0800

    Update Longest Common Prefix.cpp

--- Longest Common Prefix.cpp
+++ Longest Common Prefix.cpp
@@ -1,4 +1,31 @@
 /*
-
+挨个比较
 */
 
+class Solution {
+public:
+    string longestCommonPrefix(vector<string> &strs) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        string ret;
+        if(strs.size()==0) return ret;
+        if(strs.size()==1) return strs[0];
+        int index=0;
+        while(true)
+        {
+            int i=0;
+            char ch=strs[i][index];
+            if(ch!='\0')
+            {
+                for(i++;i<strs.size();i++)
+                {
+                    if(strs[i][index]!=ch) return ret;
+                }
+                ret+=ch;
+                index++;
+            }
+            else return ret;
+        }
+        return ret;
+    }
+};
