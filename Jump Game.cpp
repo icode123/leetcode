cbc400e50f6475dea326cddcdfc0fa7254e0cddb
icode123 <yantian_123@126.com>
2013-06-17 14:36:03 +0800

    Update Jump Game.cpp

--- Jump Game.cpp
+++ Jump Game.cpp
@@ -15,3 +15,18 @@ public:
         return maxlen>=n-1;
     }
 };
+
+class Solution {
+public:
+    bool canJump(int A[], int n) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if(A==NULL || n<=1) return true;
+        int cur=n-1;
+        for(int i=cur-1;i>=0;i--)
+        {
+            if(A[i]>=cur-i) cur=i;
+        }
+        return cur==0;
+    }
+};
