fa2f69ba3914998beed5cc364bfbd1c3b4be1daf
icode123 <yantian_123@126.com>
2013-06-17 14:46:35 +0800

    Update Valid Sudoku.cpp

--- Valid Sudoku.cpp
+++ Valid Sudoku.cpp
@@ -53,3 +53,51 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char> > &board) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        for(int i=0;i<9;i++)
+        {
+            bool flag[9]={false};
+            for(int j=0;j<9;j++)
+            {
+                if(board[i][j]!='.')
+                {
+                    if(!flag[board[i][j]-'1']) flag[board[i][j]-'1']=true;
+                    else return false;
+                }
+            }
+        }
+        for(int j=0;j<9;j++)
+        {
+            bool flag[9]={false};
+            for(int i=0;i<9;i++)
+            {
+                if(board[i][j]!='.')
+                {
+                    if(!flag[board[i][j]-'1']) flag[board[i][j]-'1']=true;
+                    else return false;
+                }
+            }
+        }
+        for(int k=0;k<9;k++)
+        {
+            bool flag[9]={false};
+            for(int i=0;i<3;i++)
+                for(int j=0;j<3;j++)
+                {
+                    int x=k/3*3+i;
+                    int y=k%3*3+j;
+                    if(board[x][y]!='.')
+                    {
+                        if(!flag[board[x][y]-'1']) flag[board[x][y]-'1']=true;
+                        else return false;
+                    }
+                }
+        }
+        return true;
+    }
+};
