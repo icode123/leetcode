13bda91100375487b9e3908f2be8bedbb3a82315
icode123 <yantian_123@126.com>
2013-06-17 14:40:33 +0800

    Update Count and Say.cpp

--- Count and Say.cpp
+++ Count and Say.cpp
@@ -33,3 +33,34 @@ class Solution {
          return s;
      }
  };
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if(n<=0) return "";
+        string ret("1");
+        for(int i=2;i<=n;i++)
+        {
+            string tmp;
+            int count=1;
+            char ch=ret[0];
+            for(int j=1;j<ret.size();j++)
+            {
+                if(ret[j]==ch) count++;
+                else
+                {
+                    tmp+=(char)count+'0';
+                    tmp+=ch;
+                    ch=ret[j];
+                    count=1;
+                }
+            }
+            tmp+=(char)count+'0';
+            tmp+=ch;
+            ret=tmp;
+        }
+        return ret;
+    }
+};
