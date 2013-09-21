b3998a4a170c0b1d2d534f046d6c33274699cef7
icode123 <yantian_123@126.com>
2013-09-21 21:46:22 +0800

    Update Palindrome Partitioning II.cpp

--- Palindrome Partitioning II.cpp
+++ Palindrome Partitioning II.cpp
@@ -22,3 +22,37 @@ public:
 		return ret[num];
 	}
 };
+
+class Solution {
+public:
+    int minCut(string s) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int num=s.size();
+        vector<vector<int>> cut(num,vector<int>(num,INT_MAX));
+        vector<vector<bool>> p(num,vector<bool>(num,false));
+        for(int i=0;i<num;i++)
+        {
+            p[i][i]=true;
+            cut[i][i]=0;
+        }
+        for(int l=2;l<=num;l++)
+           for(int i=0;i+l-1<num;i++)
+           {
+               int j=i+l-1;
+               if(s[i]==s[j] && (i+1==j || p[i+1][j-1]))
+               {
+                   p[i][j]=true;
+                   cut[i][j]=0;
+               }
+               else
+               {
+                   for(int k=i;k<j;k++)
+                   {
+                       cut[i][j]=min(cut[i][j],cut[i][k]+cut[k+1][j]+1);
+                   }
+               }
+           }
+        return cut[0][num-1];
+    }
+};
