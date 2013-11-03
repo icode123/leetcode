35bf9ae40eee2afc79f62b707da3d77300060344
icode123 <yantian_123@126.com>
2013-11-03 19:44:28 +0800

    Update Edit Distance.cpp

--- Edit Distance.cpp
+++ Edit Distance.cpp
@@ -12,7 +12,7 @@ public:
         for(int i=1;i<=len1;i++)
            for(int j=1;j<=len2;j++)
            {
-               if(word1[i]==word2[j]) res[i][j]=res[i-1][j-1];
+               if(word1[i-1]==word2[j-1]) res[i][j]=res[i-1][j-1];
                else res[i][j]=min(res[i-1][j-1],min(res[i][j-1],res[i-1][j]))+1;
            }
         return res[len1][len2];
