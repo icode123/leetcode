b1db57e70fadfb9fa5172abc9e6e109a11b4a1d3
icode123 <yantian_123@126.com>
2013-11-03 19:37:36 +0800

    Create Edit Distance.cpp

---
+++ Edit Distance.cpp
@@ -0,0 +1,13 @@
+int len1=word1.size();
+        int len2=word2.size();
+        vector<vector<int>> res(len1+1,vector<int>(len2+1));
+        res[0][0]=0;
+        for(int i=1;i<=len1;i++) res[i][0]=i;
+        for(int i=1;i<=len2;i++) res[0][i]=i;
+        for(int i=1;i<=len1;i++)
+           for(int j=1;j<=len2;j++)
+           {
+               if(word1[i]==word2[j]) res[i][j]=res[i-1][j-1];
+               else res[i][j]=min(res[i-1][j-1],min(res[i][j-1],res[i-1][j]))+1;
+           }
+        return res[len1][len2];
